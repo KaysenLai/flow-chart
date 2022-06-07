b421258f93db03999ef65c8ca8160d2155337932
Chaokai Lai <chaokai.lai@gmail.com>
2022-06-08 04:02:34 +0800

    feat: recursive layout

---
+++ src/modules/Panel/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+import Graph from "../Graph/Graph";
+const Panel = () => {
+  return (
+    <div className="app">
+      <Graph />
+    </div>
+  );
+};
+
+export default Panel;
