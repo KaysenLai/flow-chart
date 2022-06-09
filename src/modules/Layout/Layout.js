5edbd37136af7b401c047375018edff55cfdb797
弥久 <laichaokai.lck@alibaba-inc.com>
2022-06-09 19:59:44 +0800

    feat: flow chart layout

--- src/modules/Layout/Layout.js
+++ src/modules/Layout/Layout.js
@@ -1,24 +1,23 @@
-import { Graph, Node, Edge, Shape } from "@antv/x6";
-import { merge } from "lodash";
+import { Graph, Node, Edge, Shape } from '@antv/x6';
+import { merge } from 'lodash';
 
-import { get } from "lodash";
-import {basicRectAttrs} from "../Node/attrs";
+import { get } from 'lodash';
+import { basicRectAttrs } from '../Node/attrs';
 
 class ShapeBase extends Shape.Rect {
   constructor(graph, props) {
     super(props);
     this.graph = graph;
 
-    const branches = get(props, "Branches");
+    const branches = get(props, 'Branches');
     if (branches) {
-      this.setAttrByPath('body/stroke', 'gray')
-      this.setAttrByPath('body/strokeDasharray', '8,4')
+      this.setAttrByPath('body/stroke', 'gray');
+      this.setAttrByPath('body/strokeDasharray', '8,4');
       const nodes = branches.map((col) => new Layout(this.graph, col));
-      this.setChildren(nodes)
-      this.setAttrByPath("body/fill", "none");
-      this.arrange(nodes)
+      this.setChildren(nodes);
+      this.setAttrByPath('body/fill', 'none');
+      this.arrange(nodes);
     }
-
   }
 
   get width() {
@@ -38,8 +37,8 @@ class ShapeBase extends Shape.Rect {
   }
 
   arrange = (nodes) => {
-    const PADDING = 20
-    const children = nodes
+    const PADDING = 20;
+    const children = nodes;
     const heightArr = children.map((item) => item.height);
     const maxHeight = Math.max(...heightArr);
     let maxWidth = 0;
@@ -54,27 +53,27 @@ class ShapeBase extends Shape.Rect {
     children.forEach((node) => {
       node.translate(PADDING, PADDING);
     });
-    this.setSize(maxWidth+2*PADDING, maxHeight + 2*PADDING);
+    this.setSize(maxWidth + 2 * PADDING, maxHeight + 2 * PADDING);
   };
 }
 
 // 注册
-Node.registry.register("rect-node", ShapeBase, true);
+Node.registry.register('rect-node', ShapeBase, true);
 
 const GAP = 30;
 
-const GAP_HEIGHT  =  100
+const GAP_HEIGHT = 100;
 const SIZE_OFFSET = 40;
 class RectNode extends ShapeBase {
   constructor(graph, props) {
     const DEFAULT = {
       width: 200,
-      height: 50
+      height: 50,
     };
     const mergedOpt = merge(DEFAULT, props);
     const { width, height } = mergedOpt;
     const attrs = basicRectAttrs(mergedOpt);
-    super(graph,{ width, height, ...attrs });
+    super(graph, { width, height, ...attrs });
   }
 }
 class Layout extends ShapeBase {
@@ -82,7 +81,7 @@ class Layout extends ShapeBase {
     super(graph, props);
     this.graph = graph;
     const { StartAt, States } = props;
-    this.setAttrByPath('body/stroke', 'none')
+    this.setAttrByPath('body/stroke', 'none');
 
     // this.setAttrByPath('body/stroke', 'gray')
     // this.setAttrByPath('body/strokeDasharray', '8,4')
@@ -90,8 +89,8 @@ class Layout extends ShapeBase {
     this.arrangeCol();
   }
   arrangeCol = () => {
-    const PADDING =  20
-    const children = this.getChildren()
+    const PADDING = 20;
+    const children = this.getChildren();
     const widthArr = children.map((item) => item.width);
     const maxWidth = Math.max(...widthArr);
     let maxHeight = 0;
@@ -107,8 +106,12 @@ class Layout extends ShapeBase {
       const offsetX = (maxWidth - node.width) / 2;
       node.translate(offsetX + PADDING, PADDING);
     });
-    const size= { width: maxWidth + 2 * PADDING, height: maxHeight+ 2* PADDING };
+    const size = { width: maxWidth + 2 * PADDING, height: maxHeight + 2 * PADDING };
     this.setSize(size.width, size.height);
+    // 添加连线
+    for (let i = 0; i < children.length - 1; i++) {
+      this.graph.addEdge({ source: children[i], target: children[i + 1] });
+    }
   };
   addToGraph = (startAt, states) => {
     const nodeStates = [];
@@ -126,7 +129,6 @@ class Layout extends ShapeBase {
     this.graph.addNode(this);
     nodeStates.forEach((item) => {
       if (item.Type === 'Task' || item.Type === 'Pass') {
-
         const shape = new RectNode(this.graph, item);
         this.graph.addNode(shape);
         this.addChild(shape);
@@ -141,91 +143,91 @@ class Layout extends ShapeBase {
   };
 }
 
-
 const aws = {
-  StartAt: "Lambda Invoke",
+  StartAt: 'Lambda Invoke',
   States: {
-    "Lambda Invoke": {
-      Type: "Task",
-      Next: "Lambda Invoke (1)",
+    'Lambda Invoke': {
+      Type: 'Task',
+      Next: 'Lambda Invoke (1)',
     },
-    "Lambda Invoke (1)": {
-      Type: "Task",
-      Next: "Lambda Invoke (2)",
+    'Lambda Invoke (1)': {
+      Type: 'Task',
+      Next: 'Lambda Invoke (2)',
     },
-    "Lambda Invoke (2)": {
+    'Lambda Invoke (2)': {
       End: true,
     },
   },
 };
 const aws2 = {
-  StartAt: "Parallel",
+  StartAt: 'Parallel',
   States: {
     Parallel: {
-      Type: "Parallel",
+      Type: 'Parallel',
       Branches: [
         {
-          StartAt: "Lambda Invoke (1)",
+          StartAt: 'Lambda Invoke (1)',
           States: {
-            "Lambda Invoke (1)": {
-              Type: "Task",
-              Next: "Pass",
+            'Lambda Invoke (1)': {
+              Type: 'Task',
+              Next: 'Pass',
             },
             Pass: {
-              Type: "Pass",
+              Type: 'Pass',
               End: true,
             },
           },
         },
         {
-          StartAt: "Lambda Invoke (2)",
+          StartAt: 'Lambda Invoke (2)',
           States: {
-            "Lambda Invoke (2)": {
-              Type: "Task",
-              Next: "Pass (1)",
+            'Lambda Invoke (2)': {
+              Type: 'Task',
+              Next: 'Pass (1)',
             },
-            "Pass (1)": {
-              Type: "Pass",
-              Next: "Parallel2",
+            'Pass (1)': {
+              Type: 'Pass',
+              Next: 'Parallel2',
             },
             Parallel2: {
-              Type: "Parallel",
+              Type: 'Parallel',
               Branches: [
                 {
-                  StartAt: "Lambda Invoke (1)",
+                  StartAt: 'Lambda Invoke (1)',
                   States: {
-                    "Lambda Invoke (1)": {
-                      Type: "Task",
-                      Next: "Pass",
+                    'Lambda Invoke (1)': {
+                      Type: 'Task',
+                      Next: 'Pass',
                     },
                     Pass: {
-                      Type: "Pass",
+                      width: 300,
+                      Type: 'Pass',
                       End: true,
                     },
                   },
                 },
                 {
-                  StartAt: "Lambda Invoke (2)",
+                  StartAt: 'Lambda Invoke (2)',
                   States: {
-                    "Lambda Invoke (2)": {
-                      Type: "Task",
-                      Next: "Pass (1)",
+                    'Lambda Invoke (2)': {
+                      Type: 'Task',
+                      Next: 'Pass (1)',
                     },
-                    "Pass (1)": {
-                      Type: "Pass",
-                      Next: "Pass (2)",
+                    'Pass (1)': {
+                      Type: 'Pass',
+                      Next: 'Pass (2)',
                     },
-                    "Pass (2)": {
-                      Type: "Pass",
+                    'Pass (2)': {
+                      Type: 'Pass',
                       End: true,
                     },
                   },
                 },
               ],
-              Next: "Pass (2)",
+              Next: 'Pass (2)',
             },
-            "Pass (2)": {
-              Type: "Pass",
+            'Pass (2)': {
+              Type: 'Pass',
               End: true,
             },
           },
@@ -234,35 +236,35 @@ const aws2 = {
       Next: 'Parallel2',
     },
     Parallel2: {
-      Type: "Parallel",
+      Type: 'Parallel',
       Branches: [
         {
-          StartAt: "Lambda Invoke (1)",
+          StartAt: 'Lambda Invoke (1)',
           States: {
-            "Lambda Invoke (1)": {
-              Type: "Task",
-              Next: "Pass",
+            'Lambda Invoke (1)': {
+              Type: 'Task',
+              Next: 'Pass',
             },
             Pass: {
-              Type: "Pass",
+              Type: 'Pass',
               End: true,
             },
           },
         },
         {
-          StartAt: "Lambda Invoke (2)",
+          StartAt: 'Lambda Invoke (2)',
           States: {
-            "Lambda Invoke (2)": {
-              Type: "Task",
+            'Lambda Invoke (2)': {
+              Type: 'Task',
               width: 300,
-              Next: "Pass (1)",
+              Next: 'Pass (1)',
             },
-            "Pass (1)": {
-              Type: "Pass",
-              Next: "Pass (2)",
+            'Pass (1)': {
+              Type: 'Pass',
+              Next: 'Pass (2)',
             },
-            "Pass (2)": {
-              Type: "Pass",
+            'Pass (2)': {
+              Type: 'Pass',
               End: true,
             },
           },
@@ -273,46 +275,46 @@ const aws2 = {
   },
 };
 const aws3 = {
-  StartAt: "Parallel",
+  StartAt: 'Parallel',
   States: {
     Parallel: {
-      Type: "Parallel",
+      Type: 'Parallel',
       Branches: [
         {
-          StartAt: "Lambda Invoke (1)",
+          StartAt: 'Lambda Invoke (1)',
           States: {
-            "Lambda Invoke (1)": {
-              Type: "Task",
-              Next: "Pass",
+            'Lambda Invoke (1)': {
+              Type: 'Task',
+              Next: 'Pass',
             },
             Pass: {
-              Type: "Pass",
+              Type: 'Pass',
               End: true,
             },
           },
         },
         {
-          StartAt: "Lambda Invoke (1)",
+          StartAt: 'Lambda Invoke (1)',
           States: {
-            "Lambda Invoke (1)": {
-              Type: "Task",
-              Next: "Pass",
+            'Lambda Invoke (1)': {
+              Type: 'Task',
+              Next: 'Pass',
             },
             Pass: {
-              Type: "Pass",
+              Type: 'Pass',
               End: true,
             },
           },
         },
         {
-          StartAt: "Lambda Invoke (1)",
+          StartAt: 'Lambda Invoke (1)',
           States: {
-            "Lambda Invoke (1)": {
-              Type: "Task",
-              Next: "Pass",
+            'Lambda Invoke (1)': {
+              Type: 'Task',
+              Next: 'Pass',
             },
             Pass: {
-              Type: "Pass",
+              Type: 'Pass',
               End: true,
             },
           },
@@ -320,7 +322,6 @@ const aws3 = {
       ],
       End: true,
     },
-
   },
 };
 function main(graph) {
@@ -328,6 +329,5 @@ function main(graph) {
   graph.addNode(wrap);
   // const rect1 = new RectNode(graph, { color:'red'})
   // graph.addNode(rect1)
-
 }
 export { main };
